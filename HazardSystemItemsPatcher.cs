d6e599169d9a609d0a6c5a4f4a2d27dac4c696dd
Sebastian Hjorth <sebastian@shjorth.dk>
2026-06-20 20:46:10 +0200

    Item bugfixes

--- HazardSystemItemsPatcher.cs
+++ HazardSystemItemsPatcher.cs
@@ -75,13 +75,13 @@ public class HazardSystemItemsPatcher
     private void PatchInternal()
     {
         var radiationRestoreMf = AddSoakRestoreMagicEffect("Radiation", "HS_Restore_Radiation_Soak", "", "Restore <mag> of radiation suit integrity");
-        AddItem("Radiation Mesh Shield", "HS_Item_Restore_Radiation", "A lead-alloy mesh designed to soak up radiation, its nano-crystalline structure keeping excess weight to a minimum.", radiationRestoreMf, new RadiationMesh());
+        AddItem("Radiation Mesh Shield", "HS_Item_Restore_Radiation", "A lead-alloy mesh designed to soak up radiation, its nano-crystalline structure keeps excess weight to a minimum.", radiationRestoreMf, new RadiationMesh());
         var thermalRestoreMf = AddSoakRestoreMagicEffect("Thermal", "HS_Restore_Radiation_Soak", "", "Restore <mag> of thermal suit integrity");
         AddItem("Cryo Battery Pack", "HS_Item_Restore_Thermal", "A combined coolant and battery replacement for suit thermal regulators. Engineered to keep its charge under extreme heat - so you can keep your cool.", thermalRestoreMf, new ItemBatteryPack());
         var corrosiveRestoreMf = AddSoakRestoreMagicEffect("Corrosive", "HS_Restore_Corrosive_Soak", "", "Restore <mag> of corrosive suit integrity");
         AddItem("Deimos CorroGuard", "HS_Item_Restore_Corrosive", "A thick, flowing paste that provides a protective coating. Wear proper protective equipment when applied. May stain materials. Seek nearest medical facility if inhaled.", corrosiveRestoreMf, new BucketOfPaste());
         var airborneRestoreMf = AddSoakRestoreMagicEffect("Airborne", "HS_Restore_Airborne_Soak", "", "Restore <mag> of airborne suit integrity");
-        AddItem("Outland Airfilter", "HS_Item_Restore_Airborne", "A replacement filter for standard issue spacesuits. Breathe the antartic air of old.", airborneRestoreMf, new BucketOfPaste());
+        AddItem("Outland Airfilter", "HS_Item_Restore_Airborne", "A replacement filter for standard issue spacesuits. Breathe the antartic air of old.", airborneRestoreMf, new AirSieve());
     }
 
     private Ingestible AddItem(string itemName, string editorId, string description, MagicEffect effect, IItemAssets assets)
@@ -90,6 +90,7 @@ public class HazardSystemItemsPatcher
         newItem.Name = itemName;
         newItem.Description = description;
         newItem.Weight = 5;
+        newItem.Value = 4000;
         
         newItem.ConsumeSound = assets.GetUseSound(baseGameLinkCache);
 
